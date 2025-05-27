f1caf9b833f49d275a095fabee56fa3e548fbf09
Viacheslav Andreev <110542225+AndreevVE@users.noreply.github.com>
2025-05-27 14:22:47 +0300

    Update log_monitor.sh

--- log_monitor.sh
+++ log_monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 Log_dir="/var/log"
-
 report_file=/tmp/log_report.txt
-
 error="(error|ERROR|Error|warning|Warning|WARNING)"
+
+echo "Chech dir $Log_dir"
 
 sudo grep -r -E "$error" $Log_dir | awk -F ':' '{print $2}' | sort | uniq -c | sort -nr > $report_file    
 echo "Log report generated at $report_file"
